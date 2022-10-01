ea4e61e82916d615391d0f28706f395918ac7c29
erobert <etiennerobert33@gmail.com>
2022-10-01 13:57:56 +0300

    Create a menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
-import './App.css';
 import { Route, Routes } from 'react-router';
+
 import Capture from './Capture';
+import Menu from './Menu';
+import Process from './Process';
+
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Routes>
-          <Route path={'capture'} element={<Capture />} />
+          <Route path="capture" element={<Capture />} />
+          <Route path="process" element={<Process />} />
+          <Route path="*" element={<Menu />} />
         </Routes>
       </header>
     </div>
