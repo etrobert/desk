74b7ee8adfb89944999c9d6af5be1f0e90ea4a85
erobert <etiennerobert33@gmail.com>
2022-10-11 22:15:32 +0200

    Add space around TaskList

--- src/engage/TaskList.tsx
+++ src/engage/TaskList.tsx
@@ -1,11 +1,13 @@
 import ButtonLink from '../ButtonLink';
 import useTasks from '../data/useTasks';
 
+import './TaskList.css';
+
 const TaskList = () => {
   const tasks = useTasks();
   if (tasks === null) return <>Loading...</>;
   return (
-    <ul>
+    <ul className="TaskList">
       {tasks.map(({ id, title }) => (
         <li key={id}>
           <ButtonLink to={id}>{title}</ButtonLink>
