6f6a8364bb3e7dea88ed000036216e9439e0529b
erobert <etiennerobert33@gmail.com>
2022-10-19 15:53:51 +0200

    Store email and password in state

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,15 +1,29 @@
+import { useState } from 'react';
+
 import Button from './components/Button';
 import TextInput from './components/TextInput';
 
 import './Login.css';
 
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
     <>
       <h1>Login</h1>
       <form className="Login__form">
-        <TextInput placeholder="Email" />
-        <TextInput placeholder="Password" />
+        <TextInput
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+          placeholder="Email"
+        />
+        <TextInput
+          type="password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+          placeholder="Password"
+        />
         <Button type="submit">Login</Button>
       </form>
     </>
