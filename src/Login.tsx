f750dfc2a3d8bb6f02aaf90b090d7d70895643ed
erobert <etiennerobert33@gmail.com>
2022-10-19 15:51:01 +0200

    Create Login mockup

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,5 +1,19 @@
+import Button from './components/Button';
+import TextInput from './components/TextInput';
+
+import './Login.css';
+
 const Login = () => {
-  return <>Login</>;
+  return (
+    <>
+      <h1>Login</h1>
+      <form className="Login__form">
+        <TextInput placeholder="Email" />
+        <TextInput placeholder="Password" />
+        <Button type="submit">Login</Button>
+      </form>
+    </>
+  );
 };
 
 export default Login;
