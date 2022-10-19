540ffb65d8d9e3f7f212cea51efa36b72cc1f951
erobert <etiennerobert33@gmail.com>
2022-10-19 17:25:59 +0200

    Create useAuth hook

--- src/useAuth.ts
+++ src/useAuth.ts
@@ -1,12 +1,23 @@
 import { onAuthStateChanged, User } from 'firebase/auth';
 import { useEffect, useState } from 'react';
 
-import { auth } from './firebase';
+import { auth as firebaseAuth } from './firebase';
+
+type Auth = {
+  initialized: boolean;
+  user: User | null;
+};
 
 const useAuth = () => {
-  const [user, setUser] = useState<User | null>(null);
-  useEffect(() => onAuthStateChanged(auth, setUser), []);
-  return user;
+  const [auth, setAuth] = useState<Auth>({ initialized: false, user: null });
+  useEffect(
+    () =>
+      onAuthStateChanged(firebaseAuth, (user) =>
+        setAuth({ initialized: true, user })
+      ),
+    []
+  );
+  return auth;
 };
 
 export default useAuth;
