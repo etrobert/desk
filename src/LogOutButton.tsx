b79dd37ba2fea7607f1008f19269e76bdc146b64
erobert <etiennerobert33@gmail.com>
2022-10-19 18:09:48 +0200

    Add a Log Out button

---
+++ src/LogOutButton.tsx
@@ -0,0 +1,14 @@
+import { signOut } from 'firebase/auth';
+
+import Button from './components/Button';
+import { auth } from './firebase';
+
+import './LogOutButton.css';
+
+const LogOutButton = () => (
+  <Button onClick={() => signOut(auth)} className="LogOutButton">
+    Log Out
+  </Button>
+);
+
+export default LogOutButton;
--- src/SignedInPage.tsx
+++ src/SignedInPage.tsx
@@ -3,12 +3,14 @@ import Menu from './Menu';
 import Process from './process';
 import Engage from './engage';
 import HomeButton from './HomeButton';
+import LogOutButton from './LogOutButton';
 import { Route, Routes } from 'react-router';
 
 function SignedInPage() {
   return (
     <>
       <HomeButton />
+      <LogOutButton />
       <Routes>
         <Route path="capture" element={<Capture />} />
         <Route path="process/*" element={<Process />} />
