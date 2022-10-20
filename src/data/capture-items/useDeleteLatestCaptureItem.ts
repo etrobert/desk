2ce961247e729e9dc138caa9559a414ac26094ac
erobert <etiennerobert33@gmail.com>
2022-10-20 21:08:57 +0200

    Update useDeleteLatestCaptureItem with userPath

--- src/data/capture-items/useDeleteLatestCaptureItem.ts
+++ src/data/capture-items/useDeleteLatestCaptureItem.ts
@@ -1,15 +1,19 @@
 import { deleteDoc, doc } from 'firebase/firestore';
 import { useCallback } from 'react';
 import { firestore } from '../../firebase';
+import useUserPath from '../useUserPath';
 import useLatestCaptureItem from './useLatestCaptureItem';
 
 const useDeleteLatestCaptureItem = () => {
   const { latestCaptureItem } = useLatestCaptureItem();
+  const userPath = useUserPath();
 
   return useCallback(async () => {
-    if (latestCaptureItem === null) return;
-    await deleteDoc(doc(firestore, 'capture-items', latestCaptureItem.id));
-  }, [latestCaptureItem]);
+    if (latestCaptureItem === null || userPath === null) return;
+    await deleteDoc(
+      doc(firestore, ...userPath, 'capture-items', latestCaptureItem.id)
+    );
+  }, [userPath, latestCaptureItem]);
 };
 
 export default useDeleteLatestCaptureItem;
