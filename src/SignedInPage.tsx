a997b0c4fa53208f0a0bb6846596229bcef6d880
erobert <etiennerobert33@gmail.com>
2022-10-21 19:48:05 +0200

    Add UserEmail

--- src/SignedInPage.tsx
+++ src/SignedInPage.tsx
@@ -1,16 +1,19 @@
+import { Route, Routes } from 'react-router';
+
 import Capture from './Capture';
 import Menu from './Menu';
 import Process from './process';
 import Engage from './engage';
 import HomeButton from './HomeButton';
 import LogOutButton from './LogOutButton';
-import { Route, Routes } from 'react-router';
+import UserEmail from './UserEmail';
 
 function SignedInPage() {
   return (
     <>
       <HomeButton />
       <LogOutButton />
+      <UserEmail />
       <Routes>
         <Route path="capture" element={<Capture />} />
         <Route path="process/*" element={<Process />} />
---
+++ src/UserEmail.tsx
@@ -0,0 +1,10 @@
+import useAuth from './useAuth';
+
+import './UserEmail.css';
+
+const UserEmail = () => {
+  const { user } = useAuth();
+  return user && <p className="UserEmail">{user.email}</p>;
+};
+
+export default UserEmail;
