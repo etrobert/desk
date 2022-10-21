f8a93f24c03c96530e14d7168e6a63a87395256a
erobert <etiennerobert33@gmail.com>
2022-10-21 18:30:40 +0200

    Update useDeleteTask

---
+++ src/data/tasks/useDeleteTask.ts
@@ -0,0 +1,16 @@
+import { deleteDoc, doc } from 'firebase/firestore';
+import { useCallback } from 'react';
+import { firestore } from '../../firebase';
+import useUserPath from '../useUserPath';
+
+const useDeleteTask = () => {
+  const userPath = useUserPath();
+  return useCallback(
+    async (id: string) => {
+      userPath && (await deleteDoc(doc(firestore, ...userPath, 'tasks', id)));
+    },
+    [userPath]
+  );
+};
+
+export default useDeleteTask;
