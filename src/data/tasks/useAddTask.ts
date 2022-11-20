9890e354c72c916b5c068c0792aa7b535c494dac
erobert <etiennerobert33@gmail.com>
2022-11-20 17:18:13 +0100

    Simplify useAddTask

--- src/data/tasks/useAddTask.ts
+++ src/data/tasks/useAddTask.ts
@@ -1,20 +1,24 @@
 import { collection, addDoc, serverTimestamp } from 'firebase/firestore';
 import { useCallback } from 'react';
 import { firestore } from '../../firebase';
-import type { Task } from '../../types';
 import useUserPath from '../useUserPath';
+
+import type { Timestamp } from 'firebase/firestore';
+import type { Task } from '../../types';
 
 const useAddTask = () => {
   const userPath = useUserPath();
 
   return useCallback(
-    async (task: Omit<Task, 'id' | 'createdAt'>) => {
+    async (task: { title: string }) => {
+      if (!userPath) return;
+      const newTask: Omit<Task, 'id'> = {
+        ...task,
+        // serverTimestamp is not typed correctly in the firebase package
+        createdAt: serverTimestamp() as Timestamp,
+      };
       // Add a new document with a generated id.
-      userPath &&
-        (await addDoc(collection(firestore, ...userPath, 'tasks'), {
-          ...task,
-          createdAt: serverTimestamp(),
-        }));
+      await addDoc(collection(firestore, ...userPath, 'tasks'), newTask);
     },
     [userPath]
   );
