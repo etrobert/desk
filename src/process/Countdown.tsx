1f28c5032caf91c211d0adbebaa4fe303cd131f1
erobert <etiennerobert33@gmail.com>
2022-11-25 00:19:43 +0100

    Fix ntime formatting in process/do

--- src/process/Countdown.tsx
+++ src/process/Countdown.tsx
@@ -1,15 +1,15 @@
 import useCountdown from '../hooks/useCountdown';
 
+const formatNumber = (num: number) =>
+  Math.floor(num).toString().padStart(2, '0');
+
+const formatTime = (time: number) =>
+  `${formatNumber(time / 60)}:${formatNumber(time % 60)}`;
+
 const Countdown = () => {
   const time = useCountdown(2 * 60);
-  const minutes = Math.floor(time / 60);
-  const seconds = time % 60;
 
-  return (
-    <p>
-      {minutes}:{seconds}
-    </p>
-  );
+  return <p>{formatTime(time)}</p>;
 };
 
 export default Countdown;
