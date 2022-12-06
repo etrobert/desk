dbd246433bbf58e6d20df06f22081cd6e3f0aaf9
erobert <etiennerobert33@gmail.com>
2022-12-06 19:36:17 +0100

    Add button to set tags

--- src/Menu.tsx
+++ src/Menu.tsx
@@ -2,11 +2,14 @@ import Button from './components/Button';
 import ButtonGroup from './components/ButtonGroup';
 import ButtonLink from './components/ButtonLink';
 import useCaptureItemsCount from './data/capture-items/useCaptureItemsCount';
+import useSetTags from './data/tasks/useSetTags';
 import useTasks from './data/tasks/useTasks';
 
 const Menu = () => {
   const itemsCount = useCaptureItemsCount();
   const tasks = useTasks();
+
+  const setTags = useSetTags();
 
   if (itemsCount === null) return <>Loading...</>;
 
@@ -26,6 +29,7 @@ const Menu = () => {
           <ButtonLink to="engage">Engage</ButtonLink>
         )}
         <Button disabled>Reflect</Button>
+        <Button onClick={setTags}>Set Tags</Button>
       </ButtonGroup>
     </>
   );
