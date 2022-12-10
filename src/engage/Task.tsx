ef1231486a3953c826ddba8d6a097d6540cb19f3
erobert <etiennerobert33@gmail.com>
2022-12-10 18:05:47 +0100

    Add tags in Task view

--- src/engage/Task.tsx
+++ src/engage/Task.tsx
@@ -3,10 +3,13 @@ import { Navigate, useParams } from 'react-router-dom';
 import BackButton from '../components/BackButton';
 import Button from '../components/Button';
 import ButtonGroup from '../components/ButtonGroup';
+import TagList from '../components/TagList';
 
 import useUpdateTask from '../data/tasks/useUpdateTask';
 import useDeleteTask from '../data/tasks/useDeleteTask';
 import useTask from '../data/tasks/useTask';
+
+import './Task.css';
 
 const Task = () => {
   const { id } = useParams<'id'>();
@@ -17,18 +20,21 @@ const Task = () => {
   if (id === undefined || task === undefined) return <Navigate to=".." />;
   if (task === null) return <>Loading...</>;
 
-  const { title } = task;
+  const { title, tags } = task;
 
   return (
     <>
-      <p>{title}</p>
-      <ButtonGroup>
-        <Button onClick={() => updateTask({ ...task, status: 'done' })}>
-          Done
-        </Button>
-        <Button onClick={() => deleteTask(id)}>Delete</Button>
-        <BackButton>Back</BackButton>
-      </ButtonGroup>
+      <div className="Task__content">
+        <p className="Task__title">{title}</p>
+        <TagList tags={tags} />
+        <ButtonGroup>
+          <Button onClick={() => updateTask({ ...task, status: 'done' })}>
+            Done
+          </Button>
+          <Button onClick={() => deleteTask(id)}>Delete</Button>
+          <BackButton>Back</BackButton>
+        </ButtonGroup>
+      </div>
     </>
   );
 };
