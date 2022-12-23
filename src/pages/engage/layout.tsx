ded418a27cf29091db469739df0c27f83d2af8b5
erobert <etiennerobert33@gmail.com>
2022-12-23 17:11:40 +0100

    Extract task edit page in pages

--- src/pages/engage/layout.tsx
+++ src/pages/engage/layout.tsx
@@ -2,13 +2,13 @@ import { Route, Routes } from 'react-router';
 
 import TaskPage from './[id]/page';
 import EngagePage from './page';
-import EngageTaskEdit from '../../engage/EngageTaskEdit';
+import TaskEditPage from './[id]/edit/page';
 
 const EngageLayout = () => (
   <>
     <h1>Engage</h1>
     <Routes>
-      <Route path="/:id/edit" element={<EngageTaskEdit />} />
+      <Route path="/:id/edit" element={<TaskEditPage />} />
       <Route path="/:id" element={<TaskPage />} />
       <Route index element={<EngagePage />} />
     </Routes>
