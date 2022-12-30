7a7960d5834bd8a888c94b88702cec5441f49c6c
erobert <etiennerobert33@gmail.com>
2022-12-30 14:41:29 +0100

    Rename pages folder to app

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
-import AppLayout from './pages/layout';
+import AppLayout from './app/layout';
 
 import './index.css';
 
