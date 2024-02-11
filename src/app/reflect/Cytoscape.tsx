39af06e245c04deae1b959b97282e27eb0847100
erobert <etiennerobert33@gmail.com>
2024-02-11 17:44:22 +0100

    Use react-cytoscapejs

--- src/app/reflect/Cytoscape.tsx
+++ src/app/reflect/Cytoscape.tsx
@@ -1,65 +1,49 @@
-import { useEffect, useRef } from 'react';
-import cytoscape from 'cytoscape';
+import CytoscapeComponent from 'react-cytoscapejs';
 
-const setupCytoscape = (ref: React.RefObject<HTMLDivElement>) => {
-  if (ref.current === null) return;
-  cytoscape({
-    container: ref.current,
-
-    elements: [
-      // list of graph elements to start with
-      {
-        // node a
-        data: { id: 'a' },
-      },
-      {
-        // node b
-        data: { id: 'b' },
-      },
-      {
-        // edge ab
-        data: { id: 'ab', source: 'a', target: 'b' },
-      },
-    ],
-
-    style: [
-      // the stylesheet for the graph
-      {
-        selector: 'node',
-        style: {
-          'background-color': '#666',
-          label: 'data(id)',
-        },
-      },
-
-      {
-        selector: 'edge',
-        style: {
-          width: 3,
-          'line-color': '#ccc',
-          'target-arrow-color': '#ccc',
-          'target-arrow-shape': 'triangle',
-          'curve-style': 'bezier',
-        },
-      },
-    ],
-
-    layout: {
-      name: 'grid',
-      rows: 1,
+const stylesheet = [
+  // the stylesheet for the graph
+  {
+    selector: 'node',
+    style: {
+      'background-color': '#666',
+      label: 'data(id)',
     },
-  });
-};
+  },
+
+  {
+    selector: 'edge',
+    style: {
+      width: 3,
+      'line-color': '#ccc',
+      'target-arrow-color': '#ccc',
+      'target-arrow-shape': 'triangle',
+      'curve-style': 'bezier',
+    },
+  },
+];
+
+const elements = [
+  {
+    // node a
+    data: { id: 'a' },
+  },
+  {
+    // node b
+    data: { id: 'b' },
+  },
+  {
+    // edge ab
+    data: { id: 'ab', source: 'a', target: 'b' },
+  },
+];
 
 const Cytoscape = () => {
-  const ref = useRef<HTMLDivElement>(null);
-
-  useEffect(() => setupCytoscape(ref), []);
-
   return (
-    <div
-      style={{ width: '900px', height: '900px', textAlign: 'start' }}
-      ref={ref}
+    <CytoscapeComponent
+      stylesheet={stylesheet}
+      layout={{ name: 'grid', rows: 1 }}
+      style={{ width: '800px', height: '800px', textAlign: 'start' }}
+      elements={elements}
     />
   );
 };
