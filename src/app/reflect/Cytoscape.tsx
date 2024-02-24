46896bdb50d43e5ac469b80ba77a60a0ff93cec0
erobert <etiennerobert33@gmail.com>
2024-02-24 11:39:07 +0100

    Extract cytoscape stylesheet

--- src/app/reflect/Cytoscape.tsx
+++ src/app/reflect/Cytoscape.tsx
@@ -3,37 +3,14 @@ import CytoscapePackage from 'cytoscape';
 import type { ElementDefinition } from 'cytoscape';
 import CytoscapeComponent from 'react-cytoscapejs';
 import dagre from 'cytoscape-dagre';
+import stylesheet from './cytoscapeStylesheet';
 
 CytoscapePackage.use(dagre);
-
-const stylesheet = [
-  // the stylesheet for the graph
-  {
-    selector: 'node',
-    style: {
-      'background-color': '#666',
-      color: '#fff',
-      label: 'data(title)',
-    },
-  },
-
-  {
-    selector: 'edge',
-    style: {
-      width: 3,
-      'line-color': '#ccc',
-      'target-arrow-color': '#ccc',
-      'target-arrow-shape': 'triangle',
-      'curve-style': 'bezier',
-    },
-  },
-];
 
 const layout = {
   name: 'dagre',
   rankDir: 'LR',
   nodeDimensionsIncludeLabels: true,
-  animate: true,
 };
 
 const CytoscapeView = ({ elements }: { elements: ElementDefinition[] }) => {
