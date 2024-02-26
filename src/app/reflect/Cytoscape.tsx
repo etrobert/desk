406f582a6ab2b1bc1d5b90164f8aa9722669d19e
erobert <etiennerobert33@gmail.com>
2024-02-26 18:58:24 +0100

    Navigate to task page on node click

--- src/app/reflect/Cytoscape.tsx
+++ src/app/reflect/Cytoscape.tsx
@@ -1,6 +1,7 @@
-import { useRef } from 'react';
+import { useEffect, useRef } from 'react';
 import CytoscapePackage from 'cytoscape';
 import CytoscapeComponent from 'react-cytoscapejs';
+import { useNavigate } from 'react-router-dom';
 import dagre from 'cytoscape-dagre';
 import stylesheet from './cytoscapeStylesheet';
 import Button from '../../components/Button';
@@ -19,6 +20,13 @@ const layout = {
 const CytoscapeView = () => {
   const elements = useCytoscapeElements();
   const cyRef = useRef<cytoscape.Core | null>(null);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    cyRef.current?.on('tap', 'node', (node) => {
+      navigate(`/engage/${node.target.data('id')}`)
+    })
+  }, [navigate])
 
   if (elements === null) return <>Loading...</>;
 
