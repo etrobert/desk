b5c12952d0b40bbadce2842c491342a9d478acac
Etienne Robert <etiennerobert33@gmail.com>
2024-03-09 16:04:15 +0100

    Add a quick add task button to the Reflect page

--- src/app/reflect/page.tsx
+++ src/app/reflect/page.tsx
@@ -1,5 +1,11 @@
+import QuickAddTask from '@/components/QuickAddTask';
 import Cytoscape from './Cytoscape';
 
-const Reflect = () => <Cytoscape />;
+const Reflect = () => (
+  <>
+    <Cytoscape />
+    <QuickAddTask />
+  </>
+);
 
 export default Reflect;
---
+++ src/components/QuickAddTask.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+import useAddTask from '@/data/tasks/useAddTask';
+import { Button } from './ui/button';
+import { Input } from './ui/input';
+
+const QuickAddTask = () => {
+  const addTask = useAddTask();
+
+  return (
+    <form
+      className="flex gap-2 items-center"
+      onSubmit={(event) => {
+        event.preventDefault();
+        const form = event.target as HTMLFormElement;
+        const title = new FormData(form).get('title');
+        if (typeof title !== 'string' || title === '') return;
+        addTask({ title, tags: [], dependencies: [] });
+      }}
+    >
+      <Input name="title" />
+      <Button variant="secondary">Add</Button>
+    </form>
+  );
+};
+
+export default QuickAddTask;
