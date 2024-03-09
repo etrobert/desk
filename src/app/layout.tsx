89614b5b0be34951816ca4545ef13dde7a289b2b
Etienne Robert <etiennerobert33@gmail.com>
2024-03-09 14:37:20 +0100

    Remove default colors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import InnerLayout from './InnerLayout';
 import { Inter } from 'next/font/google';
 
+import { cn } from '@/lib/utils';
+
 import '../index.css';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -9,7 +11,7 @@ import type { PropsWithChildren } from 'react';
 
 function RootLayout({ children }: PropsWithChildren) {
   return (
-    <html className={['h-full', inter.className].join(' ')} lang="en">
+    <html className={cn('h-full', inter.className)} lang="en">
       <head>
         <meta charSet="utf-8" />
         <link rel="icon" href="/favicon.svg" />
@@ -22,7 +24,7 @@ function RootLayout({ children }: PropsWithChildren) {
       </head>
       <body className="h-full">
         <div className="h-full" id="root">
-          <div className="h-full text-center antialiased bg-slate-800 flex flex-col items-center justify-center text-slate-50 text-2xl gap-4">
+          <div className="h-full text-center antialiased flex flex-col items-center justify-center text-2xl gap-4">
             <InnerLayout>{children}</InnerLayout>
           </div>
         </div>
