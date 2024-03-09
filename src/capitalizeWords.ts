7ba4ca548121633f45e4c38034a46710a19d2498
Etienne Robert <etiennerobert33@gmail.com>
2024-03-09 23:07:01 +0100

    Remove unused files

--- src/capitalizeWords.ts
+++
@@ -1,6 +0,0 @@
-const capitalize = (s: string) => `${s[0].toUpperCase()}${s.substr(1)}`;
-const mapWords = (modifier: (word: string) => string) => (sentence: string) =>
-  sentence.split(' ').map(modifier).join(' ');
-const capitalizeWords = mapWords(capitalize);
-
-export default capitalizeWords;
