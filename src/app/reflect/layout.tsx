b277be9f3f3251970da941312395fcd49350286a
Etienne Robert <etiennerobert33@gmail.com>
2024-03-09 14:25:49 +0100

    Fix spacing in reflect layout

--- src/app/reflect/layout.tsx
+++ src/app/reflect/layout.tsx
@@ -3,10 +3,10 @@ import { TypographyH1 } from '@/components/ui/typography';
 import type { PropsWithChildren } from 'react';
 
 const Reflect = ({ children }: PropsWithChildren) => (
-  <div>
+  <>
     <TypographyH1>Reflect</TypographyH1>
     {children}
-  </div>
+  </>
 );
 
 export default Reflect;
