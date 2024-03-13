469e66b73fd2161ebd01f5900406729d1310b6ef
Etienne Robert <etiennerobert33@gmail.com>
2024-03-13 23:04:36 +0100

    Invalidate when deleting a task

--- src/db/deleteTask.ts
+++ src/db/deleteTask.ts
@@ -3,12 +3,14 @@
 import { eq } from 'drizzle-orm';
 import { dependencies, tasks } from '@/db/schema';
 import { db } from './db';
+import { revalidatePath } from 'next/cache';
 
 async function deleteTask(id: number) {
   // TODO: Use a transaction
   await db.delete(dependencies).where(eq(dependencies.taskId, id));
   await db.delete(dependencies).where(eq(dependencies.dependencyId, id));
-  return await db.delete(tasks).where(eq(tasks.id, id));
+  await db.delete(tasks).where(eq(tasks.id, id));
+  revalidatePath('/');
 }
 
 export default deleteTask;
