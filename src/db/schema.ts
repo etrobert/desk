e508882c8729e0e27f9b2145d8c55015f5c62033
Etienne Robert <etiennerobert33@gmail.com>
2024-03-13 14:41:03 +0100

    Add createdAt to the tasks table

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,4 +1,4 @@
-import { pgTable, serial, text } from 'drizzle-orm/pg-core';
+import { pgTable, serial, text, timestamp } from 'drizzle-orm/pg-core';
 
 export const users = pgTable('users', {
   id: serial('id').primaryKey(),
@@ -11,4 +11,5 @@ export const tasks = pgTable('tasks', {
   userId: serial('user_id')
     .references(() => users.id)
     .notNull(),
+  createdAt: timestamp('created_at').defaultNow().notNull(),
 });
