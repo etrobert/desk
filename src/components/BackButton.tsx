e3d32d58a1043c289d69d416e1fb4461d3875b56
Etienne Robert <etiennerobert33@gmail.com>
2024-03-14 19:05:08 +0100

    Simplify the BackButton code

--- src/components/BackButton.tsx
+++ src/components/BackButton.tsx
@@ -7,9 +7,8 @@ import { Button } from '@/components/ui/button';
 
 type Props = Omit<ComponentProps<typeof Button>, 'onClick'>;
 
-const BackButton = (props: Props) => {
-  const router = useRouter();
-  return <Button onClick={() => router.back()} {...props} />;
-};
+const BackButton = (props: Props) => (
+  <Button onClick={useRouter().back} {...props} />
+);
 
 export default BackButton;
