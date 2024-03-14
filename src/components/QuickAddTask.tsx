0bab28c6795f7595588db4aee4292b8c6ac3d3ec
Etienne Robert <etiennerobert33@gmail.com>
2024-03-14 23:03:11 +0100

    Use plus icon for QuickAdd

--- src/components/QuickAddTask.tsx
+++ src/components/QuickAddTask.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useEffect, useRef, useTransition } from 'react';
+import { Plus } from 'lucide-react';
 import { Button } from './ui/button';
 import { Input } from './ui/input';
 import createTaskAction from '@/actions/createTaskAction';
@@ -18,7 +19,7 @@ const QuickAddTask = () => {
   return (
     <form
       ref={ref}
-      className="absolute bottom-0 m-4 flex items-center gap-2"
+      className="absolute bottom-0 m-4 grid grid-flow-col gap-2"
       action={async (formData) => {
         startTransition(() => createTaskAction(formData));
         ref.current?.reset();
@@ -28,11 +29,12 @@ const QuickAddTask = () => {
         ref={inputRef}
         required
         name="title"
+        className="h-10"
         placeholder={isPending ? 'Adding...' : undefined}
         disabled={isPending}
       />
-      <Button variant="secondary" type="submit">
-        Add
+      <Button variant="secondary" size="icon" type="submit">
+        <Plus />
       </Button>
     </form>
   );
