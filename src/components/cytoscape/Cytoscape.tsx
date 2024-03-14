3821c97a9e832a4db299912bb53621c924589682
Etienne Robert <etiennerobert33@gmail.com>
2024-03-14 19:44:58 +0100

    Add cytoscape edgehandles

--- src/components/cytoscape/Cytoscape.tsx
+++ src/components/cytoscape/Cytoscape.tsx
@@ -2,14 +2,15 @@
 
 import { useCallback, useEffect, useRef } from 'react';
 
-import { useRouter } from 'next/navigation';
 import CytoscapePackage, { ElementDefinition } from 'cytoscape';
 import CytoscapeComponent from 'react-cytoscapejs';
 import dagre from 'cytoscape-dagre';
+import edgehandles from 'cytoscape-edgehandles';
 import stylesheet from './cytoscapeStylesheet';
 import { Button } from '@/components/ui/button';
 
 CytoscapePackage.use(dagre);
+CytoscapePackage.use(edgehandles);
 
 const layout = {
   name: 'dagre',
@@ -19,29 +20,29 @@ const layout = {
 
 const CytoscapeView = ({ elements }: { elements: ElementDefinition[] }) => {
   const cyRef = useRef<cytoscape.Core | null>(null);
-  const router = useRouter();
+  const ehRef = useRef<cytoscapeEdgehandles.EdgeHandlesInstance>(null);
 
   useEffect(() => {
-    cyRef.current?.on('tap', 'node', (node) => {
-      router.push(`/engage/${node.target.data('id')}`);
-    });
-  }, [router]);
+    // @ts-expect-error No idea what's going on here
+    ehRef.current = cyRef.current?.edgehandles();
+  }, []);
 
   const runLayout = useCallback(() => {
     cyRef.current?.layout(layout).run();
+  }, []);
+
+  const toggleDrawMode = useCallback(() => {
+    if (!ehRef.current) return;
+    const eh = ehRef.current;
+    // @ts-expect-error edgehandles types are wrong
+    if (eh.drawMode) eh.disableDrawMode();
+    else eh.enableDrawMode();
   }, []);
 
   useEffect(runLayout, [runLayout, elements]);
 
   return (
     <>
-      <Button
-        className="absolute right-0 top-0 m-4"
-        variant="outline"
-        onClick={runLayout}
-      >
-        Reset Layout
-      </Button>
       <CytoscapeComponent
         className="h-full w-screen"
         cy={(cy) => (cyRef.current = cy)}
@@ -49,6 +50,20 @@ const CytoscapeView = ({ elements }: { elements: ElementDefinition[] }) => {
         layout={layout}
         elements={elements}
       />
+      <Button
+        className="absolute right-0 top-0 m-4"
+        variant="outline"
+        onClick={runLayout}
+      >
+        Reset Layout
+      </Button>
+      <Button
+        className="absolute left-0 top-0 m-4"
+        variant="outline"
+        onClick={toggleDrawMode}
+      >
+        Draw
+      </Button>
     </>
   );
 };
