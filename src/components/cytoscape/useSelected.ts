290e4281a6f046cf8f0c9d6e32a1823182280138
Etienne Robert <etiennerobert33@gmail.com>
2024-03-15 00:24:21 +0100

    Unselect on remove

--- src/components/cytoscape/useSelected.ts
+++ src/components/cytoscape/useSelected.ts
@@ -1,8 +1,14 @@
-import { NodeSingular } from 'cytoscape';
+import { EventObject, NodeSingular } from 'cytoscape';
 import { RefObject, useEffect, useState } from 'react';
 
 const useSelected = (cyRef: RefObject<cytoscape.Core>) => {
   const [selected, setSelected] = useState<number[]>([]);
+
+  const remove = (event: EventObject) => {
+    const target = event.target as NodeSingular;
+    const id = parseInt(target.id());
+    setSelected((selected) => selected.filter((s) => s != id));
+  };
 
   useEffect(() => {
     cyRef.current?.on('select', (e) => {
@@ -10,11 +16,8 @@ const useSelected = (cyRef: RefObject<cytoscape.Core>) => {
       const id = parseInt(target.id());
       setSelected((selected) => [...new Set([...selected, id])]);
     });
-    cyRef.current?.on('unselect', (e) => {
-      const target = e.target as NodeSingular;
-      const id = parseInt(target.id());
-      setSelected((selected) => selected.filter((s) => s != id));
-    });
+    cyRef.current?.on('unselect', remove);
+    cyRef.current?.on('remove', remove);
   }, [cyRef]);
 
   return selected;
