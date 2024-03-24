9af34b790c1e279927e7edeebedb59bfb06a3f4b
Etienne Robert <etiennerobert33@gmail.com>
2024-03-24 16:00:35 +0100

    Update useSelected to omit edges

--- src/components/cytoscape/useSelected.ts
+++ src/components/cytoscape/useSelected.ts
@@ -1,23 +1,25 @@
-import { EventObject, NodeSingular } from 'cytoscape';
 import { RefObject, useEffect, useState } from 'react';
+
+const getSelectedNodes = (cy: cytoscape.Core) =>
+  cy.nodes(':selected').map((node) => parseInt(node.id()));
 
 const useSelected = (cyRef: RefObject<cytoscape.Core>) => {
   const [selected, setSelected] = useState<number[]>([]);
 
-  const remove = (event: EventObject) => {
-    const target = event.target as NodeSingular;
-    const id = parseInt(target.id());
-    setSelected((selected) => selected.filter((s) => s != id));
-  };
-
   useEffect(() => {
-    cyRef.current?.on('select', (e) => {
-      const target = e.target as NodeSingular;
-      const id = parseInt(target.id());
-      setSelected((selected) => [...new Set([...selected, id])]);
-    });
-    cyRef.current?.on('unselect', remove);
-    cyRef.current?.on('remove', remove);
+    const cy = cyRef.current;
+    if (cy === null) return;
+    const updateSelected = () => setSelected(getSelectedNodes(cy));
+
+    cy.addListener('select', updateSelected);
+    cy.addListener('unselect', updateSelected);
+    cy.addListener('remove', updateSelected);
+
+    return () => {
+      cy.removeListener('select', updateSelected);
+      cy.removeListener('unselect', updateSelected);
+      cy.removeListener('remove', updateSelected);
+    };
   }, [cyRef]);
 
   return selected;
